68dc10a10a4e6190a949c0051a96388391f49230
Øystein H. Vikane <hvikane94@gmail.com>
2021-02-24 22:23:30 +0100

    Maven init
    Created maven project

---
+++ fplm-app/src/main/java/no/fplmonkey/fplmapp/FplMonkeyWebappApplication.java
@@ -0,0 +1,13 @@
+package no.fplmonkey.fplmapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FplMonkeyWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FplMonkeyWebappApplication.class, args);
+	}
+
+}
---
+++ fplm-app/src/test/java/no/fplmonkey/fplmapp/FplMonkeyWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package no.fplmonkey.fplmapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FplMonkeyWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
