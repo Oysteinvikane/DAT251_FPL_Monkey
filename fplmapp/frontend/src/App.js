a4936ea67a8a4b52d4a92487400f746a499a73f2
Øystein H. Vikane <hvikane94@gmail.com>
2021-03-15 18:42:35 +0100

    testing CI/CD

--- fplmapp/frontend/src/App.js
+++ fplmapp/frontend/src/App.js
@@ -22,12 +22,20 @@ class App extends Component {
         return (
             <div className="App">
                 <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
+                    <img src={logo} className="App-logo" alt="logo" />
                     <h1 className="App-title">{this.state.message}</h1>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload. You should be able to see the changes straight away.
+            </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
                 </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
             </div>
         );
     }
