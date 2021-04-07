e42c56e7414b075ab3aa5c6fbc3b58f5fd66f3ac
Kjetil Dyrland <kjetil.dyrland@gmail.com>
2021-04-07 18:56:10 +0200

    players and playerName

--- notebook/app.py
+++ notebook/app.py
@@ -6,17 +6,26 @@ from requests import get
 
 app = Flask(__name__)
 
-
-
 path = Path('./')
 
-@app.route('/playerPP',methods=['GET'])
+@app.route('/playerPP', methods=['GET'])
 def player():
-    #player = request.fore['player']
-    preds = pd.read_csv(path/'data/predictions_for_season_1920_gw_20.csv')
-    arr = np.array(['Martin Kelly'])
-    player_prediction = preds[preds['player'] == arr[0]].predicted_points.to_numpy()
-    return player_prediction[0]
+    name = request.args.get('name')
+    df = pd.read_csv(path/'data/predictions_for_season_1920_gw_20.csv', index_col=0)
+    if name : 
+        arr = np.array([name])
+    else : 
+        arr = np.array(['Martin Kelly'])
+    player_prediction = df[df['player'] == arr[0]].predicted_points
+    return player_prediction.to_json()
+
+@app.route('/players', methods=['GET'])
+def getPlayers():
+    df = pd.read_csv(path/'data/predictions_for_season_1920_gw_20.csv')
+    players = df['player']
+    return players.to_json()
+
+
 
 
 
