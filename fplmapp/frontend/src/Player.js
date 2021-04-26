1ff635fb532b7042e9ba2e5c90daf8b9e4755638
Oskar Midbøe <182851@stud.hvl.no>
2021-04-26 22:10:58 +0200

    1234

--- fplmapp/frontend/src/Player.js
+++ fplmapp/frontend/src/Player.js
@@ -1,37 +1,10 @@
-<<<<<<< HEAD
-import React, {useState, useEffect} from 'react';
-=======
 import React, { useEffect } from 'react';
->>>>>>> main
 import styled from 'styled-components';
 import logo from './logos/fplm_logo.png';
 import { useParams } from "react-router-dom";
 import { useState } from 'react';
 const GridWrapper = styled.div`
   background:#f3f0e6
-<<<<<<< HEAD
-  margin:80px 0px 0px 0px
-`; 
-
-export const Player = ({name}) => {
-  const [msg, setMsg] = useState('');
-
-    const encodedvalue = encodeURIComponent(name)
-    fetch('playerpp?name=' + encodedvalue)
-    .then(response => response.text())
-    .then(message => {
-    setMsg(message);
-    console.log("loaded")
-  });
-
-  return(
-    
-    <GridWrapper >
-      <h2>{msg}</h2>
-    </GridWrapper>
-  );
-  }
-=======
   `;
 
 
@@ -60,4 +33,3 @@ export const Player = ({name}) => {
 };
 
 export default Player;
->>>>>>> main
