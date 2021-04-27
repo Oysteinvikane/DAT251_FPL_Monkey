5a493c3c740cb705f03fe13822dbfcf2d900812d
Kjetil Dyrland <kjetil.dyrland@gmail.com>
2021-04-27 12:35:19 +0200

    more stats

--- fplmapp/frontend/src/Player.js
+++ fplmapp/frontend/src/Player.js
@@ -10,26 +10,44 @@ const GridWrapper = styled.div`
 
 export const Player = ({name}) => {
 
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState([]);
   const [playerName, setPlayerName] = useState(name);
 
   const encodedvalue = encodeURIComponent(name)
     fetch('/flask/playerPP?name=' + encodedvalue)
-    .then(response => response.text())
+    .then(response => response.json())
     .then(message => {
-    setMessage(message);
-    console.log("loaded")
+    setMessage(...message);
+    console.log('loaded')
   });
-    
+
+  if (message.length == 0) { //empty message
+    return ( 
+      <header className="Home-header" >
+        <img src={logo} className="Home-logo" alt="logo" />
+          <p>
+            Search for a player at the top
+          </p>
+      </header >);
+  } else {
   return ( 
     <header className="Home-header" >
       <img src={logo} className="Home-logo" alt="logo" />
-
-
         <p>
-          {message}
-        </p>   
+          Player: {message.player}
+        </p>
+        <p>
+          Season: {message.season} Gameweek: {message.gw}
+        </p>
+        <p>
+          Opponent next gameweek: {message.opponent_team}
+        </p>
+        <p>
+          Predicted points for next gameweek: {message.predicted_points}
+        </p>
     </header >);
+  }
 };
+
 
 export default Player;
